b0986815ee411a9c31bc41cbceaeca5e44461e9b
Rene-design <151048341+Rene-design@users.noreply.github.com>
2023-11-16 18:07:21 +0300

    Groupwork.java

--- Jambo.java
+++ Jambo.java
@@ -1,5 +1,7 @@
-public class Jambo{
+public class Groupwork{
 public static void main(String[]args){
-System.out.println("Jambo");
+System.out.println("This is Java and we are going to work in groups of 4`s.");
+  System.out.println("THE MEMBERS ARE.");
+  System.out.println("RENE");
 }
 }
